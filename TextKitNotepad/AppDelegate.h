8f4037a10609a41dbb4a00961b061209734d4dad
nathanchen <qche7764@uni.sydney.edu.au>
2015-03-19 13:31:30 +0800

    Initial Commit

---
+++ TextKitNotepad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextKitNotepad
+//
+//  Created by Nate on 19/03/2015.
+//  Copyright (c) 2015 Nathan CHEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextKitNotepad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextKitNotepad
+//
+//  Created by Nate on 19/03/2015.
+//  Copyright (c) 2015 Nathan CHEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
